fc9a5a9cfbdbc3141eb59e013f968bb57490d065
coderick14 <bhattacharyya.rick14@gmail.com>
2018-05-20 23:45:24 +0530

    Populate initial data in MySQL on server startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO `issue_types` (`id`, `issue_description`) VALUES
+(1, 'ModifiersOrderCheck : Modifiers should be declared in the correct order'),
+(2, 'Collection.isEmpty() should be used to test for emptiness'),
+(3, 'Lazy initialization of static fields should be synchronized');
